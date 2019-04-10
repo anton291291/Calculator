2b955dc36814e7f198339fe7444e156daa41a38b
anton291291 <anton291291@gmail.com>
2019-04-10 13:47:59 +0300

    need to add decimal functionality

--- src/App.js
+++ src/App.js
@@ -99,82 +99,65 @@ class App extends Component {
       number2: []
     }
     this.handleClick = this.handleClick.bind(this)
-    this.add = this.add.bind(this)
-    this.substruct = this.substract.bind(this)
-    this.multiply = this.multiply.bind(this)
-    this.devide = this.devide.bind(this)
-    this.clear = this.clear.bind(this)
-    this.decimal = this.decimal.bind(this)
-    this.equals = this.equals.bind(this)
+  
   }
 
     handleClick(e) {
-      if (this.state.number.length === 1 && this.state.number == 0  ) {
+      if (this.state.number2.length == 0 && this.state.number == 0 && (e.target.value !== "0" && e.target.value !== "-" && e.target.value !== "+" &&
+      e.target.value !== "/" && e.target.value !== "*" && e.target.value !== "="
+      && e.target.value !== "."
+      && e.target.value !== "AC") )
+       {
         this.setState({
           number: [e.target.value],
           number2: [e.target.value]
         });
-      if (this.state.number.length === 1 && this.state.number !== 0 && (e.target.value !== 0 ||e.target.value == "-" || e.target.value == "+" ||
-        e.target.value == "/" ||e.target.value == "*" ||e.target.value == "="||e.target.value == "AC")  ) {
+
+      }
+      else if ((this.state.number2.length == 1 &&  (e.target.value !== 0 && e.target.value !== "-" && e.target.value !== "+" &&
+      e.target.value !== "/" && e.target.value !== "*" && e.target.value !== "="
+      && e.target.value !== "."
+      && e.target.value !== "AC")))
+       {
         this.setState({
-          number: [e.target.value],
-          number2: [e.target.value]
+          number: this.state.number.concat(e.target.value),
+          number2: this.state.number2.concat(e.target.value)
+        });
+      }
+      else if (this.state.number2.length >= 1 && (e.target.value == "-" || e.target.value == "+" ||
+      e.target.value == "/" || e.target.value == "*")) {
+        this.setState({
+          number: [e.target.value] ,
+          number2: this.state.number2.concat(e.target.value)
         });
       }
       else if
-        (this.state.number.length > 1 && (e.target.value !== "-" || e.target.value !== "+" ||
-        e.target.value !== "/" ||e.target.value !== "*" ||e.target.value !== "="
-        ||e.target.value !== "."
-        ||e.target.value !== "AC"))
+        (this.state.number2.length > 1 && (e.target.value !== "-" && e.target.value !== "+" &&
+        e.target.value !== "/" && e.target.value !== "*" && e.target.value !== "="
+        && e.target.value !== "."
+        && e.target.value !== "AC"))
         {
         this.setState({
           number: this.state.number.concat(e.target.value),
           number2: this.state.number2.concat(e.target.value)
         });
       }
-      else if (this.state.number.length > 1 && (e.target.value == "-" || e.target.value == "+" ||
-        e.target.value == "/" ||e.target.value == "*" ||e.target.value == "="))
-       {
+      else if (e.target.value == "AC") {
         this.setState({
-          number: [e.target.value],
-          number2: this.state.number2.concat(this.state.number)
+          number: [0],
+          number2: []
         });
       }
-      else {
+      else if (this.state.number.length > 1 && e.target.value == "=") {
+        const equal = eval(this.state.number2.join(""))
         this.setState({
-          number: this.state.number.concat(e.target.value),
-          number2: this.state.number2.concat(this.state.number)
+          number: equal,
+          number2: equal
         });
       }
     }
 
-    add() {
 
-    }
-
-    substract() {
-
-    }
-
-    multiply() {
-
-    }
-
-    devide() {
-
-    }
-
-    decimal() {
-
-    }
-
-    clear() {
-
-    }
-
-    equals() {
-
-    }
 
   render() {
     const allButtons= button.map((i) => {
